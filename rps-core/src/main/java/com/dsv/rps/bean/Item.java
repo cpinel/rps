6eeadfae78dddebc0b607e9509a0d957c1a36aa8
cpinel <claude.pinel@gmail.com>
2020-06-08 10:38:27 +0200

    added beans for pdf , changed location of chargeline

---
+++ rps-core/src/main/java/com/dsv/rps/bean/Item.java
@@ -0,0 +1,99 @@
+package com.dsv.rps.bean;
+
+public class Item {
+
+	
+	String description="";
+	String partNumber="";
+	String hsCode="";
+	String customerOrderNo="";
+		String  deereOrderNo="";
+		String countryOfOrigin=""; 
+		String unitPrice="";
+		String quantity=""; 
+		String weight=""; 
+		 
+		String value="";
+
+		public String getDescription() {
+			return description;
+		}
+
+		public void setDescription(String description) {
+			this.description = description;
+		}
+
+		public String getPartNumber() {
+			return partNumber;
+		}
+
+		public void setPartNumber(String partNumber) {
+			this.partNumber = partNumber;
+		}
+
+		public String getHsCode() {
+			return hsCode;
+		}
+
+		public void setHsCode(String hsCode) {
+			this.hsCode = hsCode;
+		}
+
+		public String getCustomerOrderNo() {
+			return customerOrderNo;
+		}
+
+		public void setCustomerOrderNo(String customerOrderNo) {
+			this.customerOrderNo = customerOrderNo;
+		}
+
+		public String getDeereOrderNo() {
+			return deereOrderNo;
+		}
+
+		public void setDeereOrderNo(String deereOrderNo) {
+			this.deereOrderNo = deereOrderNo;
+		}
+
+		public String getCountryOfOrigin() {
+			return countryOfOrigin;
+		}
+
+		public void setCountryOfOrigin(String countryOfOrigin) {
+			this.countryOfOrigin = countryOfOrigin;
+		}
+
+		public String getUnitPrice() {
+			return unitPrice;
+		}
+
+		public void setUnitPrice(String unitPrice) {
+			this.unitPrice = unitPrice;
+		}
+
+		public String getQuantity() {
+			return quantity;
+		}
+
+		public void setQuantity(String quantity) {
+			this.quantity = quantity;
+		}
+
+		public String getWeight() {
+			return weight;
+		}
+
+		public void setWeight(String weight) {
+			this.weight = weight;
+		}
+
+		public String getValue() {
+			return value;
+		}
+
+		public void setValue(String value) {
+			this.value = value;
+		} 
+		
+		
+}
---
+++ rps-core/src/main/java/com/dsv/rps/bean/Package.java
@@ -0,0 +1,5 @@
+package com.dsv.rps.bean;
+
+public class Package {
+
+}
